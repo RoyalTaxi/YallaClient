8bf24a06998d85ec4f3721a7c5454fa8d4cefeee
IslomSheraliyev <iamsheraliyev@gmail.com>
2024-12-11 10:22:15 +0500

    feat(GetCardList): implemented shared logic

---
+++ shared/src/commonMain/kotlin/uz/ildam/technologies/yalla/feature/payment/data/repository/CardListRepositoryImpl.kt
@@ -0,0 +1,4 @@
+package uz.ildam.technologies.yalla.feature.payment.data.repository
+
+class CardListRepositoryImpl {
+}
---
+++ shared/src/commonMain/kotlin/uz/ildam/technologies/yalla/feature/payment/domain/usecase/GetCardListUseCase.kt
@@ -0,0 +1,4 @@
+package uz.ildam.technologies.yalla.feature.payment.domain.usecase
+
+class GetCardListUseCase {
+}
