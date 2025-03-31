f53f242791da6e4a68daa13580581441da4d2370
Xursandbek <xursandbek06082001@mail.com>
2025-03-31 23:33:11 +0500

    ContactUsScreen

--- feature/contact/presentation/src/main/java/uz/yalla/client/feature/contact/components/ContactUsCard.kt
+++ feature/contact/presentation/src/main/java/uz/yalla/client/feature/contact/components/ContactUsCard.kt
@@ -1,11 +1,10 @@
 package uz.yalla.client.feature.contact.components
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -14,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -27,16 +27,16 @@ fun ContactUsCard(
     onClick: () -> Unit
 ) {
     Card(
-        shape = RoundedCornerShape(30.dp),
-        colors = CardDefaults.cardColors(YallaTheme.color.gray2),
+        shape = RectangleShape,
+        colors = CardDefaults.cardColors(YallaTheme.color.white),
         modifier = modifier.fillMaxWidth(),
         onClick = onClick
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(12.dp),
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
             modifier = Modifier
-                .padding(20.dp)
+                .padding(vertical = 18.dp, horizontal = 20.dp)
                 .fillMaxWidth()
         ) {
             Icon(
@@ -44,7 +44,7 @@ fun ContactUsCard(
                 tint = Color.Unspecified,
                 contentDescription = null,
                 modifier = Modifier
-                    .size(64.dp)
+                    .size(24.dp)
             )
 
             Text(
