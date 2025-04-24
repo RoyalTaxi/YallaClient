3ff8fd82c075537325ace48392c7dc6632eeea67
IslomSheraliyev <iamsheraliyev@gmail.com>
2025-04-24 11:01:12 +0500

    Prevent firebase from collecting errors on debug mode
    Signed-off-by: IslomSheraliyev <iamsheraliyev@gmail.com>

--- app/src/main/java/uz/yalla/client/app/App.kt
+++ app/src/main/java/uz/yalla/client/app/App.kt
@@ -2,11 +2,13 @@ package uz.yalla.client.app
 
 import android.app.Application
 import com.google.android.gms.maps.MapsInitializer
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.jakewharton.threetenabp.AndroidThreeTen
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 import org.koin.java.KoinJavaComponent
+import uz.yalla.client.BuildConfig
 import uz.yalla.client.core.data.di.Common
 import uz.yalla.client.core.domain.local.AppPreferences
 import uz.yalla.client.di.Navigation
@@ -17,6 +19,8 @@ class App : Application() {
 
         AndroidThreeTen.init(this)
         MapsInitializer.initialize(this)
+
+        FirebaseCrashlytics.getInstance().isCrashlyticsCollectionEnabled = !BuildConfig.DEBUG
 
         startKoin {
             androidContext(this@App)
