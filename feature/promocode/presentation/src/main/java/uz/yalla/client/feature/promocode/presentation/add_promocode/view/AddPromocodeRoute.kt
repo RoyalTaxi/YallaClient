4a7d749e09f53b301698200c7db60f9c20af9b34
Islom <iamsheraliyev@gmail.com>
2025-08-12 12:33:58 +0500

    refactor(AddPromocode): Improve focus management and lifecycle handling
    - Updated `FocusRequester` initialization using `remember` for efficiency.
    - Added `repeatOnLifecycle` to ensure focus is requested only during the active lifecycle state.
    - Applied `imePadding` to `Scaffold` modifier for better keyboard handling.

--- feature/promocode/presentation/src/main/java/uz/yalla/client/feature/promocode/presentation/add_promocode/view/AddPromocodeRoute.kt
+++ feature/promocode/presentation/src/main/java/uz/yalla/client/feature/promocode/presentation/add_promocode/view/AddPromocodeRoute.kt
@@ -3,8 +3,12 @@ package uz.yalla.client.feature.promocode.presentation.add_promocode.view
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.focus.FocusRequester
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import org.koin.androidx.compose.koinViewModel
 import org.orbitmvi.orbit.compose.collectSideEffect
 import uz.yalla.client.feature.promocode.presentation.add_promocode.intent.AddPromocodeSideEffect
@@ -15,7 +19,8 @@ fun AddPromocodeRoute(
     onBack: () -> Unit,
     viewModel: AddPromocodeViewModel = koinViewModel(),
 ) {
-    val focusRequester = FocusRequester()
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val focusRequester = remember { FocusRequester() }
     val state by viewModel.container.stateFlow.collectAsStateWithLifecycle()
 
     viewModel.collectSideEffect { effect ->
@@ -24,9 +29,12 @@ fun AddPromocodeRoute(
         }
     }
 
-    LaunchedEffect(Unit) {
-        focusRequester.requestFocus()
+    LaunchedEffect(lifecycleOwner) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+            focusRequester.requestFocus()
+        }
     }
+
 
     AddPromocodeScreen(
         state = state,
